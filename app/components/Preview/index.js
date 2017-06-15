bcb2a54f50435cdcf7edac034a7a2b465524fcc9
Michael Bowman <bowmanmc@gmail.com>
2017-06-14 21:31:49 -0400

    MainScreen layout

--- app/components/Preview/index.js
+++ app/components/Preview/index.js
@@ -3,7 +3,9 @@ import React from 'react';
 
 const Preview = () => {
     return (
-        <h1>Preview</h1>
+        <div className="Preview">
+            <h1>Preview</h1>
+        </div>
     );
 };
 
--- app/screens/MainScreen.js
+++ app/screens/MainScreen.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Preview from '../components/Preview';
+import Sidebar from '../components/Sidebar';
 
 
 const MainScreen = () => {
     return (
         <div className="MainScreen">
+            <Sidebar />
             <Preview />
         </div>
     );
