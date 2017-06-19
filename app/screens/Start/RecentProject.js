eeaa016114ed55944fe47cecdec89d2a894a4362
Michael Bowman <bowmanmc@gmail.com>
2017-06-19 19:44:02 -0400

    recent project card

--- app/screens/Start/RecentProject.js
+++ app/screens/Start/RecentProject.js
@@ -4,7 +4,16 @@ import React from 'react';
 const RecentProject = (props) => {
     return (
         <div className="StartScreen__RecentProject">
-            <h2>{props.project.name}</h2>
+            <div className="StartScreen__RecentProject-preview">
+
+            </div>
+            <div className="StartScreen__RecentProject-info">
+                <div className="StartScreen__RecentProject-name">
+                    {props.project.name}
+                </div>
+                <div className="StartScreen__RecentProject-meta">
+                </div>
+            </div>
         </div>
     );
 };
