30856012cf23e65f60feeb0503b8e36ee76a16af
Michael Bowman <bowmanmc@gmail.com>
2017-06-25 10:21:49 -0400

    bigger default window size

--- application.js
+++ application.js
@@ -19,8 +19,8 @@ function createWindow() {
 
     // Create the browser window.
     mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600
+        width: 1200,
+        height: 768
     });
 
     // and load the index.html of the app.
@@ -31,7 +31,7 @@ function createWindow() {
     }));
 
     // Open the DevTools.
-    // mainWindow.webContents.openDevTools()
+    mainWindow.webContents.openDevTools()
 
     // Emitted when the window is closed.
     mainWindow.on('closed', function() {
