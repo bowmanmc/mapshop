88821a7d9db3e8be74c3b1e7f0f8a8cec098644f
Michael Bowman <bowmanmc@gmail.com>
2017-07-02 18:03:32 -0400

    load some map data

--- app/screens/MapEditor/MapEditorCanvas.js
+++ app/screens/MapEditor/MapEditorCanvas.js
@@ -1,11 +1,30 @@
+import fs from 'fs';
 import React from 'react';
 
-const MapEditorCanvas = (props) => {
-    return (
-        <div className="MapEditorCanvas">
 
-        </div>
-    );
+class MapEditorCanvas extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    loadMapData() {
+        // todo: this needs to be fixed to work in a packaged app...
+        const filepath = `${process.cwd()}/app/data/state.oh.json`;
+        console.log('Loading: ' + filepath);
+        const data = JSON.parse(fs.readFileSync(filepath, 'utf8'));
+        //console.log('Loaded data: ' + JSON.stringify(data));
+        return data;
+    }
+
+    render() {
+        const mapData = this.loadMapData();
+
+        return (
+            <div className="MapEditorCanvas">
+            </div>
+        );
+    }
 };
 
 export default MapEditorCanvas;
