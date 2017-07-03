81fc7b86e03f2b533e17c327703cae744b3d63e0
Michael Bowman <bowmanmc@gmail.com>
2017-07-03 16:28:46 -0400

    layers in the toolbar

--- app/screens/MapEditor/MapEditorToolbar.js
+++ app/screens/MapEditor/MapEditorToolbar.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
+import Logo from '../../components/Logo';
+
+
 const MapEditorToolbar = (props) => {
     return (
         <div className="MapEditorToolbar">
-            tb
+            <div className="MapEditorToolbar__logo">
+                <Logo />
+            </div>
+
+            <div className="MapEditorToolbar__item">
+                <a className="MapEditorToolbar__link">Data</a>
+            </div>
+
+            <div className="MapEditorToolbar__item">
+                <a className="MapEditorToolbar__link">Reference</a>
+            </div>
+
+            <div className="MapEditorToolbar__item">
+                <a className="MapEditorToolbar__link">Base Map</a>
+            </div>
+
+            <div className="MapEditorToolbar__item MapEditorToolbar__info">
+                v1.0.2 (2017-07)
+            </div>
         </div>
     );
 };
