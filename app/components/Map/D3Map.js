664026a5258a5d0f88c46f4e2bdacbd8347a3ba1
Michael Bowman <bowmanmc@gmail.com>
2017-08-05 19:53:03 -0400

    render states

--- app/components/Map/D3Map.js
+++ app/components/Map/D3Map.js
@@ -12,30 +12,43 @@ class D3Map extends React.Component {
         super(props);
     }
 
-    loadMapData() {
+    getFeature(features, stateId) {
+        var i, feature;
+        var len = features.length;
+        for (i = 0; i < len; i++) {
+            feature = features[i];
+            if (feature.properties.STUSPS === stateId) {
+                return feature;
+            }
+        };
+        console.log('ERROR: Couldn\'t find: ' + stateId);
+    }
+
+    loadMapData(mapId) {
         // todo: this needs to be fixed to work in a packaged app...
-        const filepath = `${process.cwd()}/app/data/state.oh.json`;
+        const filepath = `${process.cwd()}/app/data/cb_2015_us_state_500k.json`;
         console.log('Loading: ' + filepath);
-        const data = JSON.parse(fs.readFileSync(filepath, 'utf8'));
-        //console.log('Loaded data: ' + JSON.stringify(data));
+        const geojson = JSON.parse(fs.readFileSync(filepath, 'utf8'));
+
+        const data = this.getFeature(geojson.features, mapId);
         return data;
     }
 
     render() {
         const project = this.props.project;
 
-        const mapData = this.loadMapData();
+        const feature = this.loadMapData(project.basemap.mapId);
         const projection = geoConicConformal();
         const path = geoPath().projection(projection);
 
         const width = this.props.size.width;
         const height = this.props.size.height;
 
-        const centroid = geoCentroid(mapData.features[0]);
+        const centroid = geoCentroid(feature);
         const r = [centroid[0] * -1, centroid[1] * -1];
         projection.scale(1).translate([0, 0]).rotate(r);
 
-        const b = path.bounds(mapData);
+        const b = path.bounds(feature);
         const s = 0.95 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height);
         const t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
         projection.scale(s).translate(t);
@@ -56,21 +69,25 @@ class D3Map extends React.Component {
         }
         //console.log('Rendering map with styles: ' + JSON.stringify(styles));
 
-        const state = mapData.features.map((d, i) => {
-            return (
-                <path
-                    {...styles}
-                    key={'path-' + i}
-                    d={path(d)}
-                    className='state'
-                    />
-            );
-        });
+        // const state = features.map((d, i) => {
+        //     return (
+        //         <path
+        //             {...styles}
+        //             key={'path-' + i}
+        //             d={path(d)}
+        //             className='state'
+        //             />
+        //     );
+        // });
 
         return (
             <svg ref={node => this.node = node}
                 width={width} height={height}>
-                {state}
+                <path
+                    {...styles}
+                    d={path(feature)}
+                    className='state'
+                />
             </svg>
         );
     }
