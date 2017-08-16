aa2d1725d90c4af9b468cbb1ff5c30d34df3f138
Michael Bowman <bowmanmc@gmail.com>
2017-08-15 22:57:23 -0400

    Export button

--- app/components/AppToolbar/index.js
+++ app/components/AppToolbar/index.js
@@ -9,6 +9,14 @@ const AppToolbar = (props) => {
             <div className="AppToolbar__logo">
                 <Logo />
             </div>
+            <div className="AppToolbar__middle">
+            </div>
+            <div className="AppToolbar__right">
+                <a className="AppToolbar__btn">
+                    <svg><use xlinkHref="images/fa/solid.svg#share-square"></use></svg>
+                    <span>Export</span>
+                </a>
+            </div>
         </div>
     );
 };
