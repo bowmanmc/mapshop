92f86f83b8efc3cfe73b2a718ee34de61865266c
Michael Bowman <bowmanmc@gmail.com>
2017-09-16 14:14:30 -0400

    fail spec if a map doesn't render under perf budget

--- app/components/Map/SvgRenderer.perf.spec.js
+++ app/components/Map/SvgRenderer.perf.spec.js
@@ -4,18 +4,22 @@ import DataIndex from '../../data';
 import DataLoader from '../../data/loader';
 import SvgRenderer from './SvgRenderer';
 
+const BUDGET = 100; // 100ms max to render a map
 
-describe('SvgRenderer Performance', () => {
-    const BUDGET = 100; // 100ms max to render a map
-    it('renders all maps under performance budget', () => {
-        const maps = Object.keys(DataIndex);
-        let project = {
-            basemap: [{
-                fillColor: '#ffdd00',
-                stroke: null
-            }]
-        };
-        maps.forEach(mapId => {
+describe('SvgRenderer Performance Checks', () => {
+
+    // Get all maps in the Data Index and render each one with some default styles
+    // Make sure all maps render under our performance budget
+    const maps = Object.keys(DataIndex);
+    let project = {
+        basemap: [{
+            fillColor: '#ffdd00',
+            stroke: null
+        }]
+    };
+    // Test each map and fail if it doesn't meet our performance requirements
+    maps.forEach(mapId => {
+        it(`Render ${mapId} in under ${BUDGET}ms`, () => {
             const mapInfo = DataIndex[mapId];
             const mapData = DataLoader.load(mapId);
             project.basemap[0].mapId = mapId;
@@ -23,10 +27,7 @@ describe('SvgRenderer Performance', () => {
             SvgRenderer.render(project, [mapData]);
             const end = moment();
             const dur = end.diff(start);
-            if (dur > BUDGET) {
-                console.log(`Map ${mapId} is over budget! (${dur}ms)`);
-            }
+            expect(dur).toBeLessThan(BUDGET);
         });
     });
-
 });
